329948104e852bd2568c6822755529b174a4886f
Valentinus Delvin <delvinwicaksono@gmail.com>
2025-11-07 21:19:14 +0700

    chore(inventory): remove unused code on create inventory

--- app/Http/Controllers/inventoryController.php
+++ app/Http/Controllers/inventoryController.php
@@ -14,32 +14,23 @@ class inventoryController extends Controller
         return view('inventory.index')->with('inventories', $inventories);
     }
 
-    public function create()
-    {
-        return view('inventory.create');
-    }
-
     public function store(Request $request)
     {
-        // 1. Validasi data input
         $request->validate([
             'id' => 'required|unique:inventories,inventory_id',
             'name' => 'required|min:3|max:255',
         ]);
 
-        // 2. Simpan data ke database
         inventory::create([
             'inventory_id' => $request->id,
             'inventory_name' => $request->name,
         ]);
 
-        // 3. Arahkan pengguna kembali ke halaman daftar produk
         return Redirect::route('inventory.index');
     }
 
     public function delete($id)
     {
-        // Hapus data berdasarkan ID
         inventory::where('inventory_id', $id)->delete();
         return Redirect::route('inventory.index');
     }
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,5 @@ Route::get('/', function () {
 });
 
 Route::get('/inventory', [inventoryController::class, 'index'])->name('inventory.index');
-Route::get('/inventory/create', [inventoryController::class, 'create'])->name('inventory.create');
 Route::post('/inventory', [inventoryController::class, 'store'])->name('inventory.store');
 Route::delete('/inventory/{id}', [inventoryController::class, 'delete'])->name('inventory.delete');
